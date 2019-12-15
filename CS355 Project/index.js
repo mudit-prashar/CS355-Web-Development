672056b939de198f97b19d0c7d68ae58b4fce341
Mudit Prashar <Mudit.prashar69@qmail.cuny.edu>
2019-12-15 02:04:18 -0500

    Project Finished

--- CS355 Project/index.js
+++ CS355 Project/index.js
@@ -20,12 +20,11 @@ const credentials = require('./auth/credentials.json');
 const fs = require('fs');
 let state_storage = [];
 const authentication_cache = './auth/authentication-res.json';
-const img_path = './album-art/';
 
-let albums = [];
-let album_art = [];
-let downloaded_images = 0;
-let test = [];
+
+let SpotifyPlaylistNames = [];
+let NBAinfo = [];
+
 
 const new_connection = function(req,res){
     if (req.url === "/") {
@@ -40,25 +39,29 @@ const new_connection = function(req,res){
         req.on("data", function (chunk) {request_data += chunk;});
         req.on("end", function () {
             let user_input = url.parse(req.url, true).query;
-            console.log(user_input.artist);
+            
             
             let state = crypto.randomBytes(20).toString("hex");
-            state_storage.push({state, artist: user_input.artist});
+            state_storage.push({state, user_input: user_input.q});
             console.log(state_storage);
             const authorization_endpoint = "https://accounts.spotify.com/authorize";
             let uri = querystring.stringify({
                 response_type:'code',
                 client_id: credentials.client_id,
                 redirect_uri: credentials.redirect_uri,
+                scope:credentials.scope,
                 state
             });
+          
             res.writeHead(302, {Location: `${authorization_endpoint}?${uri}`});
             res.end();
         });
     }
     else if (req.url.startsWith("/return")) {
         let auth_response = url.parse(req.url, true).query;
+        
         let previous_state = state_storage.find((state_title_pair) => state_title_pair.state === auth_response.state);
+      
         if (previous_state === undefined) {
             res.writeHead(403);
             res.end("CSRF Detected, Aborting Request");
@@ -67,52 +70,54 @@ const new_connection = function(req,res){
             res.writeHead(403);
             res.end("Spotify API Access Denied");
         }
-        else {
-            let artist = previous_state.artist;
-            
-            state_storage = state_storage.filter((element) => element.state !== auth_response.state);
+        else{
+            let user_input = previous_state.user_input;
+            let auth_sent_time = new Date();
             const access_token_endpoint = "https://accounts.spotify.com/api/token";
-            let post_data = querystring.stringify({
-                client_id : credentials.client_id,
-                client_secret : credentials.client_secret,
-                grant_type : "client_credentials"
-                
-            });
-            let options = {
-                method: "POST",
-                headers: {
-                    
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Content-Length": post_data.length
-                }
+            var code = auth_response.code
+            var authOptions = querystring.stringify({
+                    client_id : credentials.client_id,
+                    client_secret : credentials.client_secret,
+                  code: code,
+                  redirect_uri: credentials.redirect_uri,
+                  grant_type: 'authorization_code',
+                  
+              });
+              let opt = {
+                        method: "POST",
+                        headers: {
+                            
+                            "Content-Type": "application/x-www-form-urlencoded",
+                            "Content-Length": authOptions.length
+                        }
             };
-                let cache_valid = false;
-                if(fs.existsSync(authentication_cache)){
-                    let cache_content = fs.readFileSync(authentication_cache, 'utf-8');
+            let cache_valid = false;
+            if(fs.existsSync(authentication_cache)){
+                let cache_content = fs.readFileSync(authentication_cache, 'utf-8');
                     cache_json = JSON.parse(cache_content);
                     if(new Date(cache_json.expiration) > Date.now()){
                         cache_valid = true;
-                    }else{
+                        }else{
                         console.log("Token expired");
-                    }
+                        }
                 }
-                if(cache_valid){
-                    console.log("already cache");
-                    create_search_req(cache_json, res, artist);
-                }else{
-                    let auth_sent_time = new Date();
-
-                    let authentication_req = https.request(access_token_endpoint, options, function (authentication_res) {
-                    received_authentication(authentication_res, artist, auth_sent_time, res );
-                    });
-                    authentication_req.on('error', function(e){
-                        console.error(e);
-                    });
-                    console.log("Requesting Token");
-                    authentication_req.end(post_data);
+            if(cache_valid){
+                console.log("already cache");
+                create_search_req(cache_json, user_input, res);
+                
                 }
-            
+            else{
+                let authentication_req= https.request(access_token_endpoint, opt, function( body) {
+                    received_auth(body, user_input,res, auth_sent_time);   
+                });
+                authentication_req.on('error', function(e){
+                    console.error(e);
+                });
+                console.log("Requesting Token");
+                authentication_req.end(authOptions);
+                }
         }
+
     }
     else  if(req.url.includes('/album-art/')){
         console.log('album-art endpoint');
@@ -135,11 +140,13 @@ const new_connection = function(req,res){
 
 };
 
-const received_authentication = function(authentication_res, artist, auth_sent_time, res){
+
+const received_auth = function(authentication_res,user_input,res, auth_sent_time){
     authentication_res.setEncoding("utf8");
     let body="";
     authentication_res.on("data", function(chunk){body += chunk;});
     authentication_res.on("end", function(){
+        
         let spotify_auth = JSON.parse(body);
         auth_sent_time.setHours(auth_sent_time.getHours()+1);
         spotify_auth.expiration = auth_sent_time;
@@ -148,10 +155,14 @@ const received_authentication = function(authentication_res, artist, auth_sent_t
         console.log(spotify_auth);
         create_cache(spotify_auth);
         console.log("new cache");
-        create_search_req(spotify_auth, res, artist);
+  
+        create_search_req(spotify_auth, user_input,res);
+                
+       
 
     })
 }
+
 const create_cache = function(spotify_auth){
     let cacheJSON = JSON.stringify(spotify_auth);
     fs.writeFile(authentication_cache, cacheJSON, (error)=>{
@@ -164,102 +175,77 @@ const create_cache = function(spotify_auth){
 
     })
 }
+const create_search_req = function(spotify_auth, user_input, res){
+    let test=""
 
-const create_search_req = function(spotify_auth, res, artist){
-    console.log(`Artist Name: ${artist}`);
-    let param = {
-        access_token : spotify_auth.access_token,
-        q : artist,
-        type : 'album',
-        limit:20
-    }
-    let search_req_url = 'https://api.spotify.com/v1/search?'+querystring.stringify(param);
-    console.log(search_req_url);
-
-
-    let search_req = https.request(search_req_url, function(search_res){
+    let opt = {
+        method: "GET",
+        headers: {
+            'Authorization': 'Bearer ' + spotify_auth.access_token 
+            
+        }
+    };
+    let search_req_url = 'https://api.spotify.com/v1/me/playlists';
+   
+    let search_req = https.request(search_req_url,opt, function(search_res){
         let results = "";
         
         search_res.on('data', function(chunk){results += chunk;});
        
         search_res.on('end', function(){
-			let search_res_data = JSON.parse(results);
-           
-			for(let i=0; i < param.limit; i++ ){
+            let search_res_data = JSON.parse(results);
+            console.log(`Total Playlists Received: ${search_res_data.total}`)
+         
+                for(let i=0; i < search_res_data.total; i++ ){
+                    let name = {name: search_res_data.items[i].name}
+                    SpotifyPlaylistNames.push(name);
+            }
+                console.log(SpotifyPlaylistNames);
+                console.log("Second API Call Begins")
+
+                secondapi_call(user_input, res)
                 
-                let artist = {
-                    name: search_res_data.albums.items[i].name,
-                    image: search_res_data.albums.items[i].images[1].url
-                }
-                
-                albums.push(artist);
-                
-                download_images(albums[i], res);
-                
-			}
         })
-        console.log("second API Call Begins")
-        
-        secondapi_call(artist.charAt(0))
-       
         
     });
-    
-    
     search_req.end();
+    
+
 }
-const secondapi_call = function(firstcharacter){
+
+const secondapi_call = function(firstcharacter, res){
     let results2 = "";
- 
-    https.get(`https://www.balldontlie.io/api/v1/players?search=${firstcharacter}`, (res) => {
+    
+    let search2 = https.get(`https://www.balldontlie.io/api/v1/players?search=${firstcharacter}`, (res) => {
 
         res.on('data', (chunk) => {
             results2 += chunk;
-            // console.log(`results: ${results2}`)
+            
         });
-      
+        res.on('end', function(){
+            let SecondApiData = JSON.parse(results2);
+            console.log(`Results Received: ${SecondApiData.meta.total_count}`)
+            for(let i=0; i < SecondApiData.meta.total_count; i++ ){
+                let info = {
+                    name: SecondApiData.data[i].first_name + ' ' + SecondApiData.data[i].last_name,
+                    height:SecondApiData.data[i].height_feet,
+                    weight:SecondApiData.data[i].weight_pounds,
+                    teaminfo: SecondApiData.data[i].team
+                }
+                NBAinfo.push(info);
+                console.log(NBAinfo)
+            }
+            
+        })
       }).on('error', (e) => {
         console.error(e);
       });
+      search2.end();
+      let webpage = "Results Shown in Console"
+      res.writeHead(200,{'Content-type':'text/html'});
+      res.end(webpage);
 }
-const download_images = function (image_url, res){
-    let img_path_name = img_path + image_url.name+'.jpeg';
-    let full_img_path = `<img src="${img_path_name}">`;
-    
-    test.push(image_url.name+'.jpeg')
-    album_art.push(full_img_path);
-    
-    if( fs.existsSync(img_path_name)){
-        console.log("image already exists");
-        generate_webpage(album_art, res);
-    }
-    
-        
-	    let image_req = https.get(image_url.image, function(image_res){
-		let new_img = fs.createWriteStream(img_path_name, {'encoding':null});
-		image_res.pipe(new_img);
-		new_img.on("finish", function() {
-			downloaded_images += 1;
-			if(downloaded_images === albums.length){
-				console.log('Finished Writing Images');
-				generate_webpage(album_art, res);
-			}
-		});
-    });
 
-	image_req.on('error', function(err){console.log(err);});
-};
-const generate_webpage = function(album_art, res){
-    let webpage = "";
-
-	for (index = 0; index < album_art.length; index++) { 
-      
-         webpage += album_art[index];
-         res.writeHead(200,{'Content-type': 'text/html'});
-
-    } 
-    res.end(webpage);
-}
 
 const server = http.createServer(new_connection);
 server.listen(port, host);
